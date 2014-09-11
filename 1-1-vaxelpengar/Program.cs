27290d01802f4f61c9f4eceb9b836ac3319f8b63
vw222an <vw222an@student.lnu.se>
2014-09-11 14:51:29 +0200

    Got the math class to work.

--- 1-1-vaxelpengar/Program.cs
+++ 1-1-vaxelpengar/Program.cs
@@ -10,7 +10,34 @@ namespace _1_1_vaxelpengar
     {
         static void Main(string[] args)
         {
+            Console.Title = "Växelpengar - nivå A";
 
+            double totalSum;
+            double subTotal;
+            double roundingOffAmount;
+            int totalAmount;
+
+
+            Console.Write("Ange totalsumma  : ");
+            totalSum = double.Parse(Console.ReadLine());
+
+            Console.Write("Ange erhållet belopp : ");
+            subTotal = double.Parse(Console.ReadLine());
+
+            subTotal = (uint)Math.Round(totalSum);
+            roundingOffAmount = totalSum - subTotal;
+
+            Console.Write("KVITTO");
+
+            Console.WriteLine("Totalt: {0:c}", totalSum);
+            Console.WriteLine("Öresavrundning: {0:c}", roundingOffAmount);
+            Console.Write("Att betala: ");
+            
+
+
+
+
+            
         }
     }
 }
