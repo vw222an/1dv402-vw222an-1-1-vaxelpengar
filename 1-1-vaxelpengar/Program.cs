bac8420ff1ab5555ad96c436ff9917f9b435d3a7
vw222an <vw222an@student.lnu.se>
2014-09-17 18:29:40 +0200

    Got the cash back function to work
    You can now see how much of what kind of money you get back.

--- 1-1-vaxelpengar/Program.cs
+++ 1-1-vaxelpengar/Program.cs
@@ -18,10 +18,18 @@ namespace _1_1_vaxelpengar
             double totalAmount;
             double cash;
             double cashBack;
+            double fiveHundreds;
+            double hundreds;
+            double fifties;
+            double twenties;
+            double tens;
+            double fives;
+            double ones;
+           
 
                 Console.Write("Ange totalsumma  : ");
                 totalSum = double.Parse(Console.ReadLine());
-                
+
                 Console.Write("Ange erhållet belopp : ");
                 subTotal = int.Parse(Console.ReadLine());
                 cash = subTotal;
@@ -33,8 +41,8 @@ namespace _1_1_vaxelpengar
             totalAmount = totalSum - roundingOffAmount;
             cashBack = cash - totalAmount;
 
-            Console.Write("KVITTO");
-
+            Console.WriteLine("KVITTO");
+            
             Console.WriteLine("Totalt: {0:c}", totalSum);
             Console.WriteLine("Öresavrundning: {0:c}", roundingOffAmount);
             Console.WriteLine("Att betala: {0:c0}", totalAmount); 
@@ -43,7 +51,28 @@ namespace _1_1_vaxelpengar
 
 
 
+            fiveHundreds = cashBack / 500;
+            hundreds = cashBack % 500;
+            hundreds = hundreds / 100;
+            fifties = cashBack % 100;
+            fifties = fifties / 50;
+            twenties = cashBack % 50;
+            twenties = twenties / 20;
+            tens = cashBack % 20;
+            tens = tens / 10;
+            fives = cashBack % 10;
+            fives = fives / 5;
+            ones = cashBack % 5;
+            ones = ones / 1;
 
+
+            Console.WriteLine("Antal 500-lappar: {0}", (int)fiveHundreds);
+            Console.WriteLine("Antal 100-lappar: {0}", (int)hundreds);
+            Console.WriteLine("Antal 50-lappar: {0}", (int)fifties);
+            Console.WriteLine("Antal 20-lappar: {0}", (int)twenties);
+            Console.WriteLine("Antal 10-kronor: {0}", (int)tens);
+            Console.WriteLine("Antal 5-kronor: {0}", (int)fives);
+            Console.WriteLine("Antal 1-kronor: {0}", (int)ones);
 
         }
     }
